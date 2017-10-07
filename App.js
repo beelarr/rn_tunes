af47bf3c4355325650c27fde2eb973f98c54004c
beelarr <bryonl@me.com>
2017-10-07 16:47:21 -0500

    Project finished

--- App.js
+++ App.js
@@ -1,37 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  FlatList,
+  Image
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-export default class App extends Component<{}> {
+class Cell extends React.Component {
   render() {
     return (
+        <View style={styles.cell}>
+          <Image style={styles.imageView} source={{uri: this.props.cellItem.image[3]['#text']}} />
+          <View style={styles.contentView}>
+            <Text style={styles.whiteText}>{this.props.cellItem.name}</Text>
+
+          </View>
+          <View style={styles.accessoryView}>
+            <Text style={styles.textCenter}> > </Text>
+          </View>
+        </View>)
+  }
+}
+
+export default class App extends Component<{}> {
+
+  fetchTopTracks = () => {
+      const apiKey = "80b1866e815a8d2ddf83757bd97fdc76";
+      const url = `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`;
+      return fetch(url)
+          .then(response => response.json())
+  }
+  constructor(props){
+    console.log('constructor');
+    super(props);
+    this.state = { tracks: []};
+
+      this.fetchTopTracks()
+          .then(json => {this.setState({ tracks: json.tracks.track})
+          })
+  }
+  render() {
+
+    const data = Array(50).fill("Smells like feet and buttcrack!");
+    return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <FlatList
+            data={this.state.tracks}
+            renderItem={ ({item}) => (
+                <Cell cellItem={item}/>
+            )}
+            keyExtractor={ (_, index) => index } />
       </View>
     );
   }
@@ -40,18 +60,36 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingLeft: 15,
+    paddingRight: 15,
+  },
+  cell:{
+    height: 75,
+    marginBottom: 4,
+    flexDirection: 'row'
+  },
+  imageView:{
+    height: 75,
+    width: 75,
+    backgroundColor: 'grey'
+  },
+   contentView: {
+    flex:1,
+    backgroundColor: 'green',
+    marginLeft: 15,
+
+   },
+   accessoryView: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
+    justifyContent: 'center',
+    width: 40
+   },
+    textCenter: {
     textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    },
+    whiteText: {
+    color: 'white',
+    }
+
 });
