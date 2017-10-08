1ba7a02bd95c0ddd9ee6668e989262252ba9364a
beelarr <bryonl@me.com>
2017-10-07 20:14:16 -0500

    App built

--- App.js
+++ App.js
@@ -1,95 +1,117 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  Platform,
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  Image
+    StyleSheet,
+    Text,
+    View,
+    FlatList,
+    Image,
+    TouchableHighlight,
 } from 'react-native';
 
 class Cell extends React.Component {
-  render() {
-    return (
-        <View style={styles.cell}>
-          <Image style={styles.imageView} source={{uri: this.props.cellItem.image[3]['#text']}} />
-          <View style={styles.contentView}>
-            <Text style={styles.whiteText}>{this.props.cellItem.name}</Text>
+    handleTap() {
+        console.log('tap tap tap')
+    }
 
-          </View>
-          <View style={styles.accessoryView}>
-            <Text style={styles.textCenter}> > </Text>
-          </View>
-        </View>)
-  }
+    render() {
+        return (
+            <TouchableHighlight onPress={this.handleTap}>
+                <View style={styles.cell}>
+                    <Image
+                        style={styles.imageView }
+                        source={{uri: this.props.cellItem.image[3]['#text']}}
+                    />
+                    <View style={styles.contentView }>
+                        <Text style={[styles.whiteText, styles.textBold]}>{this.props.cellItem.name}</Text>
+                        <Text style={styles.whiteText}>{this.props.cellItem.artist.name}</Text>
+                    </View>
+                    <View style={styles.accessoryView }>
+                        <Text style={[styles.whiteText, styles.textCenter]}>></Text>
+                    </View>
+                </View>
+            </TouchableHighlight>
+        )
+    }
 }
 
-export default class App extends Component<{}> {
+export default class App extends React.Component {
+    fetchTopTracks() {
+        const apiKey = "80b1866e815a8d2ddf83757bd97fdc76"
+        const url = `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`
 
-  fetchTopTracks = () => {
-      const apiKey = "80b1866e815a8d2ddf83757bd97fdc76";
-      const url = `http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&api_key=${apiKey}&format=json`;
-      return fetch(url)
-          .then(response => response.json())
-  }
-  constructor(props){
-    console.log('constructor');
-    super(props);
-    this.state = { tracks: []};
+        return fetch(url).
+        then(response => response.json())
+    }
 
-      this.fetchTopTracks()
-          .then(json => {this.setState({ tracks: json.tracks.track})
-          })
-  }
-  render() {
+    constructor(props) {
+        super(props)
 
-    const data = Array(50).fill("Smells like feet and buttcrack!");
-    return (
-      <View style={styles.container}>
-        <FlatList
-            data={this.state.tracks}
-            renderItem={ ({item}) => (
-                <Cell cellItem={item}/>
-            )}
-            keyExtractor={ (_, index) => index } />
-      </View>
-    );
-  }
+        this.state = { tracks: [] }
+
+        // fetch api data
+        this.fetchTopTracks().
+        then(json => {
+            this.setState({ tracks: json.tracks.track })
+        })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <FlatList
+                    data={this.state.tracks}
+                    renderItem={ ({item}) => (
+                        <Cell cellItem={item} />
+                    )}
+                    keyExtractor={ (_, index) => index }
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 50,
-    paddingLeft: 15,
-    paddingRight: 15,
-  },
-  cell:{
-    height: 75,
-    marginBottom: 4,
-    flexDirection: 'row'
-  },
-  imageView:{
-    height: 75,
-    width: 75,
-    backgroundColor: 'grey'
-  },
-   contentView: {
-    flex:1,
-    backgroundColor: 'green',
-    marginLeft: 15,
-
-   },
-   accessoryView: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 40
-   },
+    container: {
+        flex: 1,
+        paddingTop: 40,
+        paddingLeft: 15,
+        paddingRight: 15,
+        backgroundColor: '#000000'
+    },
+    cell: {
+        marginBottom: 4,
+        flexDirection: 'row',
+    },
+    imageView: {
+        height: 75,
+        width: 75,
+    },
+    contentView: {
+        flex: 1,
+        justifyContent: 'center',
+        marginLeft: 15
+    },
+    accessoryView: {
+        width: 40,
+        justifyContent: 'center',
+    },
     textCenter: {
-    textAlign: 'center',
+        textAlign: 'center'
     },
     whiteText: {
-    color: 'white',
+        color: 'white'
+    },
+    textBold: {
+        fontWeight: 'bold'
     }
-
 });
+
+
+
+
+
+
+
+
+
+
